c6c2d8b46a7d0292aab6206710cc9795e5723e3e
Stelios Tsampas <zeronebones@gmail.com>
2013-05-19 16:13:27 +0300

    Pin definitions for proper SPI in mega

--- hardware/ariadne/bootloaders/ariadne/net.c
+++ hardware/ariadne/bootloaders/ariadne/net.c
@@ -17,8 +17,8 @@
 #include "debug.h"
 #include "debug_net.h"
 
-#define SS_LOW()	SPI_PORT &= ~_BV(ESS)
-#define SS_HIGH()	SPI_PORT |= _BV(ESS)
+#define SS_LOW()	ETH_PORT &= ~_BV(ETH_SS)
+#define SS_HIGH()	ETH_PORT |= _BV(ETH_SS)
 
 uint8_t registerBuffer[REGISTER_BLOCK_SIZE] = {
 	0x80,         // MR Mode - reset device
@@ -40,54 +40,67 @@ uint8_t registerBuffer[REGISTER_BLOCK_SIZE] = {
 };
 
 
+/** Send uint8_t to Ethernet controller */
 void netWriteReg(uint16_t address, uint8_t value)
 {
 	DBG_NET_EX(
-		tracePGMlnNet(mDebugNet_NWREG);
-		tracenum(address);
-		tracePGM(mDebugNet_COMMA);
-		tracenum(value);
+	    tracePGMlnNet(mDebugNet_NWREG);
+	    tracenum(address);
+	    tracePGM(mDebugNet_COMMA);
+	    tracenum(value);
 	)
 
-	// Send uint8_t to Ethernet controller
 	SPCR = _BV(SPE) | _BV(MSTR); // Set SPI as master
 	SS_LOW();
+
 	SPDR = SPI_WRITE;
 	while(!(SPSR & _BV(SPIF)));
+
 	SPDR = address >> 8;
 	while(!(SPSR & _BV(SPIF)));
+
 	SPDR = address & 0xff;
 	while(!(SPSR & _BV(SPIF)));
+
 	SPDR = value;
 	while(!(SPSR & _BV(SPIF)));
+
 	SS_HIGH();
 	SPCR = 0; // Turn off SPI
 }
 
+
+/** Read uint8_t from Ethernet controller */
 uint8_t netReadReg(uint16_t address)
 {
-#ifdef SPAM_ME
+#if defined(SPAM_ME)
 	DBG_NET_EX(
-		tracePGMlnNet(mDebugNet_NRREG);
-		tracenum(address);
+	    tracePGMlnNet(mDebugNet_NRREG);
+	    tracenum(address);
 	)
 #endif
 
-	// Read uint8_t from Ethernet controller
 	uint8_t returnValue;
+
 	SPCR = _BV(SPE) | _BV(MSTR);
 	SS_LOW();
+
 	SPDR = SPI_READ;
 	while(!(SPSR & _BV(SPIF)));
+
 	SPDR = address >> 8;
 	while(!(SPSR & _BV(SPIF)));
+
 	SPDR = address & 0xff;
 	while(!(SPSR & _BV(SPIF)));
+
 	SPDR = 0;
 	while(!(SPSR & _BV(SPIF)));
+
 	SS_HIGH();
 	returnValue = SPDR;
 	SPCR = 0;
+
 	return(returnValue);
 }
 
@@ -108,34 +121,45 @@ void netInit(void)
 {
 	uint8_t i;
 
-	/* Set up pins to communicate with W5100 chip */
-	/* Set SPI pins high */
+	/**
+	 * Set up pins to communicate with W5100 chip.
+	 * At this stage all pins are set to HIGH. This in fact DISABLES SPI for both Ethernet and SD.
+	 * SS pin for ethernet is pulled low just in time for reading or writing data inside those
+	 * functions. */
+
+	/** Set SPI pins high */
 	SPI_PORT = _BV(SCK) | _BV(MISO) | _BV(MOSI) | _BV(SS);
-	/* Set SPI pins as output */
+	/** Set SPI pins as output */
 	SPI_DDR = _BV(SCK) | _BV(MOSI) | _BV(SS);
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-	SPI_PORT |= _BV(ESS);
-	SPI_DDR |= _BV(ESS);
+
+#if (ETH_SS != SS)
+	/** Initialize extra SS pin used in some boards (mega) */
+	/** Set ethernet SS high */
+	ETH_PORT |= _BV(ETH_SS);
+	/** Set ethernet SS as output */
+	ETH_DDR |= _BV(ETH_SS);
 #endif
 
-	/* Disable SD card */
-	/* Set SD SS pin high */
+	/** Disable SD card */
+	/** Set SD SS pin high */
 	SD_PORT |= _BV(SD_SS);
-	/* Set SD SS pin as output */
+	/** Set SD SS pin as output */
 	SD_DDR |= _BV(SD_SS);
 
-	/* Set up pins to flash the onboard led */
-	/* Set led pin to high */
+#if (LED != SCK)
+	/** Set up pins to flash the onboard led */
+	/** Set led pin to high */
 	LED_PORT |= _BV(LED);
-	/* Set led pin as output */
+	/** Set led pin as output */
 	LED_DDR |= _BV(LED);
+#endif
 
 	/* Set up SPI
 	 * Set the Double SPI Speed Bit */
 	SPSR = (1 << SPI2X);
 
-	/* Pull in altered network settings
-	 * if available from AVR EEPROM (if signature bytes are set) */
+	/* Pull in altered network settings, if available,
+	 * from AVR EEPROM (if signature bytes are set) */
 	if((eeprom_read_byte(EEPROM_SIG_1) == EEPROM_SIG_1_VALUE)
 		&& (eeprom_read_byte(EEPROM_SIG_2) == EEPROM_SIG_2_VALUE)) {
 
@@ -172,7 +196,7 @@ void netInit(void)
 		}
 	)
 
-	// Configure Wiznet chip
+	/** Configure Wiznet chip. Network settings */
 	for(i = 0; i < REGISTER_BLOCK_SIZE; i++)
 		netWriteReg(i, registerBuffer[i]);
 
--- hardware/ariadne/bootloaders/ariadne/pin_defs.h
+++ hardware/ariadne/bootloaders/ariadne/pin_defs.h
@@ -1,65 +1,66 @@
 #ifndef pin_defs_h
 #define pin_defs_h
 
-/* Uno and Deumilanove */
+/** Uno and Deumilanove */
 #if defined(__AVR_ATmega328__) || defined(__AVR_ATmega328P__)
-	/* Onboard LED is connected to pin PB5 in Duemilanove and Uno or to PB1 in Ethernet */
-	#define LED_DDR     DDRB
-	#define LED_PORT    PORTB
-	#define LED_PIN     PINB
+	/** SPI port and pins */
+	#define SPI_DDR		DDRB
+	#define SPI_PORT	PORTB
+
+	#define SCK     PINB5  //Pin 13
+	#define MISO    PINB4  //Pin 12
+	#define MOSI    PINB3  //Pin 11
+	#define SS      PINB2  //Pin 10
+
+	/** Ethernet Shield SS pin */
+	#define ETH_DDR		DDRB
+	#define ETH_PORT	PORTB
+	#define ETH_SS		PINB2  //Pin 10
+
+	/** SD card SS pin */
+	#define SD_DDR	DDRD
+	#define SD_PORT	PORTD
+	#define SD_SS	PIND4  //Pin 04
+
+	/** Onboard LED is connected to pin PB5 in Duemilanove and Uno or to PB1 in Ethernet */
+	#define LED_DDR		DDRB
+	#define LED_PORT	PORTB
 	#if defined(ARDUINO_ETHERNET)
 		#define LED     PINB1  //Pin 9
 	#else
 		#define LED     PINB5  //Pin 13
 	#endif
 
-	/* SPI port and pins */
-	#define SPI_DDR		DDRB
-	#define SPI_PORT	PORTB
-	#define SPI_PIN		PINB
-
-	#define SCK     PINB5  //Pin 13
-	#define MISO    PINB4  //Pin 12
-	#define MOSI    PINB3  //Pin 11
-	#define SS      PINB2  //Pin 10
-	#define ESS		PINB2  //Pin 10
-
-	/* SD card SS pin */
-	#define SD_DDR	DDRD
-	#define SD_PORT	PORTD
-	#define SS_PIN	PIND
-	#define SD_SS	PIND4  //Pin 04
-
-/* Mega1280 and Mega2560 */
+/** Mega1280 and Mega2560 */
 #elif defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-	/* Onboard LED is connected to pin PB7 on Arduino Mega */
-	#define LED_DDR     DDRB
-	#define LED_PORT    PORTB
-	#define LED_PIN     PINB
-	#define LED         PINB7
-
-	/* SPI port and pins */
+	/** SPI port and pins */
 	#define SPI_DDR		DDRB
 	#define SPI_PORT	PORTB
-	#define SPI_PIN		PINB
-
 	#define SCK		PINB1  //Pin 52
 	#define MISO	PINB3  //Pin 50
 	#define MOSI	PINB2  //Pin 51
 	#define SS      PINB0  //Pin 53
-	#define ESS		PINB4  //Pin 10
 
-	/* SD card SS pin */
+	/** Ethernet Shield SS pin */
+	#define ETH_DDR		DDRB
+	#define ETH_PORT	PORTB
+	#define ETH_SS		PINB4  //Pin 10
+
+	/** SD card SS pin */
 	#define SD_DDR	DDRG
 	#define SD_PORT	PORTG
-	#define SD_PIN	PING
 	#define SD_SS	PING5  //Pin 04
 
-/* Leonardo */
-#elif defined(__AVR_ATmega32U4__)
+	/** Onboard LED is connected to pin PB7 on Arduino Mega */
+	#define LED_DDR     DDRB
+	#define LED_PORT    PORTB
+	#define LED         PINB7
 
+/** Leonardo */
+#elif defined(__AVR_ATmega32U4__)
+	#error "Leonardo not supported yet"
 #else
-	#error "no pin definitions for MCU available"
+	#error "No pin definitions for MCU available"
 #endif
 
 #endif
