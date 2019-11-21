5b0dadbf05fdfc99b2e48deea8c6f2867b789258
Phillip Johnston <phillip@embeddedartistry.com>
2019-11-21 10:36:51 -0800

    Allow default network settings to be overridden during the build process

--- avr/bootloaders/ariadne/src/net.h
+++ avr/bootloaders/ariadne/src/net.h
@@ -12,10 +12,21 @@
 #endif
 
 /* Network settings */
+#ifndef IP_ADDR
 #define IP_ADDR     10,0,1,199
+#endif
+
+#ifndef SUB_MASK
 #define SUB_MASK    255,255,255,0
+#endif
+
+#ifndef GW_ADDR
 #define GW_ADDR     10,0,1,1
+#endif
+
+#ifndef MAC_ADDR
 #define MAC_ADDR    0xDE,0xAD,0xBE,0xEF,0xFE,0xED
+#endif
 
 void netInit(void);
 
