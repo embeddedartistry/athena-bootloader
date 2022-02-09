08e555fa01fb3646b506bb6b74be8e37213ba350
Hagai Gold <hagaigold@gmail.com>
2022-02-09 13:30:27 +0200

    packaging for board manager

---
+++ tools/package_bm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create package for board manager (package_AB_index.json) with the required directory structure
+
+OUTPUT_DIR=./buildresults/release
+PACKAGE_TMP_DIR=./buildresults/tmp
+PACKAGE_NAME=athena-bootloader
+
+rm -rf ${PACKAGE_TMP_DIR}
+
+mkdir -p ${PACKAGE_TMP_DIR}/${PACKAGE_NAME}
+cp -r avr/* ${PACKAGE_TMP_DIR}/${PACKAGE_NAME}
+
+cd ${PACKAGE_TMP_DIR}
+zip -r $OLDPWD/${OUTPUT_DIR}/athena_bootloaders-bm.zip ${PACKAGE_NAME}
