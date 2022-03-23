d8c851ee7d973f10251bcfd8541a923e8e8cd8c3
Phillip Johnston <phillip@embeddedartistry.com>
2022-03-23 11:12:29 -0700

    Overhaul packaging process - Eliminate unneeded package script and rename package_bm to package.sh - Simplify Makefile invocation - Update package template to and package script to add the version number to the filename.

--- tools/package.sh
+++ tools/package.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
+# Create package for board manager (package_AB_index.json) with the required directory structure
 
-OUTPUT_DIR=./buildresults/release
+VERSION=`git describe --tags`
 
-# Argument 1 is the (optional) version number
+OUTPUT_DIR=buildresults/release
+PACKAGE_NAME=athena-bootloader
+OUTPUT_FILENAME=${OUTPUT_DIR}/athena_bootloaders-${VERSION}.zip
+
+# Generate .zip archive for board manager using generated artifacts
 mkdir -p ${OUTPUT_DIR}
-zip ${OUTPUT_DIR}/athena_bootloaders.zip avr/bootloaders/athena/*.hex
+mv avr ${PACKAGE_NAME}
+zip -X -r $OUTPUT_FILENAME ${PACKAGE_NAME} -x "*.DS_Store*"
+mv ${PACKAGE_NAME} avr
+
+# Store checksum and other details for the newly generated package
+CHECKSUM=`shasum -a 256 $OUTPUT_FILENAME | awk '{ print $1 }'`
+SIZE=`wc -c $OUTPUT_FILENAME | awk '{print $1 }'`
+
+# Generate the new board manager JSON file
+cat tools/package_athena-bootloader_index.template.json |
+sed s/%%VERSION%%/${VERSION}/ |
+sed s/%%VERSION%%/${VERSION}/ |
+sed s/%%CHECKSUM%%/${CHECKSUM}/ |
+sed s/%%SIZE%%/${SIZE}/ > package_athena-bootloader_index.json
