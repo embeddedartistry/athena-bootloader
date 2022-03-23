5f7c78d00d228245ceb077d9131c09e7153af6f1
Phillip Johnston <phillip@embeddedartistry.com>
2022-03-23 11:53:34 -0700

    Attempt to resolve nightly build repeats with auto-checkins.

---
+++ tools/check_nightly_build_need.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Get the description of the build commit. If the commit is in the form "X.Y.Z", we do not
+# need to run the build. If it has a "-", that indicates we have commits past the last build,
+# and we actually need to run.
+
+GIT_DESCRIBE=`git describe --tags`
+
+if [[ "$GIT_DESCRIBE" == *"-"* ]]; then
+	return 0
+fi
+
+# No need to build
+return 1
