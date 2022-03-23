2a7e52ebe6e42a7286e7c8a1864b5ff664443090
Phillip Johnston <phillip@embeddedartistry.com>
2022-03-23 10:57:05 -0700

    Update package_bm script to generate the board manager JSON file from zip contents

--- tools/package_bm.sh
+++ tools/package_bm.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
-
 # Create package for board manager (package_AB_index.json) with the required directory structure
 
-OUTPUT_DIR=./buildresults/release
-PACKAGE_TMP_DIR=./buildresults/tmp
+set -ex
+
+VERSION=$1
+
+ORIGIN_DIR=`pwd`
+OUTPUT_DIR=${ORIGIN_DIR}/buildresults/release
+PACKAGE_TMP_DIR=buildresults/tmp
 PACKAGE_NAME=athena-bootloader
+OUTPUT_FILENAME=${OUTPUT_DIR}/athena_bootloaders-bm.zip
 
+# Generate .zip archive for board manager using generated artifacts
 rm -rf ${PACKAGE_TMP_DIR}
-
 mkdir -p ${PACKAGE_TMP_DIR}/${PACKAGE_NAME}
 cp -rp avr/* ${PACKAGE_TMP_DIR}/${PACKAGE_NAME}
-
 cd ${PACKAGE_TMP_DIR}
-zip -r $OLDPWD/${OUTPUT_DIR}/athena_bootloaders-bm.zip ${PACKAGE_NAME}
+zip -X -r $OUTPUT_FILENAME ${PACKAGE_NAME} -x "*.DS_Store*"
+
+# Store checksum and other details for the newly generated package
+CHECKSUM=`shasum -a 256 $OUTPUT_FILENAME | awk '{ print $1 }'`
+SIZE=`wc -c $OUTPUT_FILENAME | awk '{print $1 }'`
+
+# Generate the new board manager JSON file
+cd ${ORIGIN_DIR}
+cat tools/package_athena-bootloader_index.template.json |
+sed s/%%VERSION%%/${VERSION}/ |
+sed s/%%CHECKSUM%%/${CHECKSUM}/ |
+sed s/%%SIZE%%/${SIZE}/ > package_athena-bootloader_index.json
