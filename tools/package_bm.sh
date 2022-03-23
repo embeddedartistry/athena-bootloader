c09c5110b72f17c7dec60ee489cbc7b9e36242bc
Phillip Johnston <phillip@embeddedartistry.com>
2022-03-23 11:05:54 -0700

    Update package_bm script to generate a deterministic zip file
    By avoiding a copy and using a temporary directory rename, we can generate a deterministic zip file - it has the same SHA each time the build is run.

--- tools/package_bm.sh
+++ tools/package_bm.sh
@@ -3,25 +3,20 @@
 
 VERSION=`git describe --tags`
 
-ORIGIN_DIR=`pwd`
-OUTPUT_DIR=${ORIGIN_DIR}/buildresults/release
-PACKAGE_TMP_DIR=buildresults/tmp
+OUTPUT_DIR=buildresults/release
 PACKAGE_NAME=athena-bootloader
 OUTPUT_FILENAME=${OUTPUT_DIR}/athena_bootloaders-bm.zip
 
 # Generate .zip archive for board manager using generated artifacts
-rm -rf ${PACKAGE_TMP_DIR}
-mkdir -p ${PACKAGE_TMP_DIR}/${PACKAGE_NAME}
-cp -rp avr/* ${PACKAGE_TMP_DIR}/${PACKAGE_NAME}
-cd ${PACKAGE_TMP_DIR}
+mv avr ${PACKAGE_NAME}
 zip -X -r $OUTPUT_FILENAME ${PACKAGE_NAME} -x "*.DS_Store*"
+mv ${PACKAGE_NAME} avr
 
 # Store checksum and other details for the newly generated package
 CHECKSUM=`shasum -a 256 $OUTPUT_FILENAME | awk '{ print $1 }'`
 SIZE=`wc -c $OUTPUT_FILENAME | awk '{print $1 }'`
 
 # Generate the new board manager JSON file
-cd ${ORIGIN_DIR}
 cat tools/package_athena-bootloader_index.template.json |
 sed s/%%VERSION%%/${VERSION}/ |
 sed s/%%CHECKSUM%%/${CHECKSUM}/ |
